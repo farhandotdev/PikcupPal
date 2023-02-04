838d94ae27d102a5aa64436bf4612bff057a1924
Aditya Joshi <adityajoshi304@gmail.com>
2023-02-04 10:27:59 +0530

    CSS fileds added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Home";
+import {Routh, Switch} from "react-router-dom";
+import Book from "./Components/Book";
+import Track from "./Components/Track";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Switch>
+          <Routh path="/" component={Home}/>
+          <Routh path="/book" component={Book}/>
+          <Routh path="/track" component={Track}/>
+        </Switch>
       </div>
     );
   }
