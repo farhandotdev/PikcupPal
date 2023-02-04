0e4c6542672a9dbcb0d13e58ed66d0d3b496de1f
Farhan Alam <farhanalam.dev@outlook.com>
2023-02-04 10:03:59 +0530

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Book from "./Components/Book";
 import Home from "./Components/Home";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Home />
-      </div>
-    );
-  }
+import Track from "./Components/Track";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="/track" element={<Track />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 export default App;
